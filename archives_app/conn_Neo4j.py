a566614e95a6ac0df2f5aa0d4f5895b1cdfa4329
lipebol <lipe_bol5@hotmail.com>
2022-12-27 21:47:12 -0300

    new connection with Neo4j

--- archives_app/conn_Neo4j.py
+++ archives_app/conn_Neo4j.py
@@ -1,22 +1,17 @@
-from py2neo import Graph
+from neo4j import GraphDatabase
 import json
+import os
 
 
-def toRecordGraphBook():
-    print("\n[Neo4j] \n")
-    userNeo4j = input("  Usuário: ")
-    passNeo4j = input("  Senha: ")
-    hostNeo4j = input("  Host: ")
-    URL = "bolt://{}:7687".format(hostNeo4j)
-    graph = Graph(URL, auth=(userNeo4j, passNeo4j))
+def queryNeo4j(query):
     file = open('book.json')
     docjson = json.load(file)
-    query = graph.run("""
+    return query.run("""
     WITH $docjson as data
-    UNWIND  data.autor AS autor
-    MERGE (p:Autor {nome: autor})
+    UNWIND  data.título AS livro
+    MERGE (p:Autor {nome: data.autor})
     MERGE (c: Coleção {nome: data.coleção})
-    MERGE (b:Livro {título: data.título})
+    MERGE (b:Livro {título: livro})
     SET b.id_MongoDB = data._id, b.páginas = data.páginas, b.idioma = data.idioma, b.dt_pub = data.data_da_publicação
     MERGE (e:Editora {nome: data.editora})
     MERGE (p)-[:ESCREVEU]->(b)
@@ -25,4 +20,19 @@ def toRecordGraphBook():
     """, docjson = docjson
     )
 
-    return print("\nCriado com Sucesso!\n")
+
+def toRecordGraphBook():
+    print("\n[Neo4j] \n")
+    userNeo4j = input("  Usuário: ")
+    passNeo4j = input("  Senha: ")
+    hostNeo4j = input("  Host: ")
+    CONNECTION_STRING = "neo4j://{}:7687".format(hostNeo4j)
+    connection = GraphDatabase.driver(CONNECTION_STRING, auth=(userNeo4j, passNeo4j))
+    with connection.session() as session:
+        session.execute_write(queryNeo4j)
+        session.close()
+    with open('book.json','w') as cnt:
+        pass
+    os.remove('book.json')
+
+    return print("\n Criado com Sucesso!\n")
