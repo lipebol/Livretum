2a6f6bccf35d5b57868c8d419dfce018ef00fcbe
lipebol <lipe_bol5@hotmail.com>
2023-05-18 13:24:43 -0300

    check if the item exists, and if it exists, update

--- app/utils/others/add_Item.py
+++ app/utils/others/add_Item.py
@@ -1,38 +1,29 @@
 # import json
 from utils.others.get_MongoDB import getMongoDB
+from utils.verifications.ack_Item import ackItem
 from utils.verifications.auth_Path import authPath
 from utils.verifications.extract_Two import extractTwo
 from utils.verifications.path_User import pathUser
+from utils.verifications.up_Item import upItem
 
 
 def addItem(URL_book, collection, status, user_bookcase):
 
-    title, authors, pub_company, pub_date, isbn_10, isbn_13, pages, categories, rating, price = extractTwo(URL_book)
-
-    item = {
-        "categoria(s)": categories,
-        "título": title,
-        "autor(es)": authors,
-        "editora": pub_company,
-        "data_da_publicação": pub_date,
-        "isbn_10": isbn_10,
-        "isbn_13": isbn_13,
-        "páginas": pages,
-        "Adquirido?": status,
-        "avaliação": rating,
-        "preço": price,
-    }
-
+    item = extractTwo(URL_book, status)
+    
     database = f"{user_bookcase}_books"
     directory, files = pathUser()
     conn_type = open(f"{directory}/.type_mongodb").read().strip()
     user, pwd, addr = authPath(directory, files)
     MongoDB = getMongoDB(conn_type, user, pwd, addr, database)
     collection = MongoDB[collection]
-    confirm_item = collection.find_one(item)
-    if confirm_item:
-        return "prev_Registered"
-    else:
+    ack_item = ackItem(
+        item["título"], 
+        item["isbn_10"], 
+        item["isbn_13"], 
+        collection
+    )
+    if ack_item == "No Exists":
         collection.insert_one(item)
         mongobook = collection.find_one(item)
         if mongobook:
@@ -40,8 +31,13 @@ def addItem(URL_book, collection, status, user_bookcase):
             # mongobook = json.dumps(mongobook, ensure_ascii=False, indent=11)
             # with open("book.json", "w") as outfile:
             #     outfile.write(mongobook)
-
             return "item_Added"
-    
-    
-    
+    else:
+        up_item = upItem(
+            ack_item, item["categoria(s)"], 
+            item["avaliação"], item["preço"], collection
+        )
+        if up_item == "No Update":
+            return "prev_Registered"
+        if up_item == "Update":
+            return "up_Registered"
