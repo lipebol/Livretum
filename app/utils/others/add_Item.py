b9430313e257d825e2aaa2f1eaa187cca7485132
lipebol <lipe_bol5@hotmail.com>
2023-05-25 13:02:23 -0300

    Add new function

--- app/utils/others/add_Item.py
+++ app/utils/others/add_Item.py
@@ -1,9 +1,7 @@
 # import json
-from utils.others.get_MongoDB import getMongoDB
 from utils.verifications.ack_Item import ackItem
-from utils.verifications.auth_Path import authPath
+from utils.verifications.conn_Database import connDatabase
 from utils.verifications.extract_Two import extractTwo
-from utils.verifications.path_User import pathUser
 from utils.verifications.up_Item import upItem
 
 
@@ -11,11 +9,7 @@ def addItem(URL_book, collection, status, user_bookcase):
 
     item = extractTwo(URL_book, status)
     
-    database = f"{user_bookcase}_books"
-    directory, files = pathUser()
-    conn_type = open(f"{directory}/.type_mongodb").read().strip()
-    user, pwd, addr = authPath(directory, files)
-    MongoDB = getMongoDB(conn_type, user, pwd, addr, database)
+    MongoDB = connDatabase(user_bookcase)
     collection = MongoDB[collection]
     ack_item = ackItem(
         item["título"], 
