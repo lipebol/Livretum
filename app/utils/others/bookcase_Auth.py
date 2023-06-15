5445674e4ee0f302b35d7be2c0e7cec9e926ab8c
lipebol <lipe_bol5@hotmail.com>
2023-06-15 19:36:34 -0300

    Cleaning

--- app/utils/others/bookcase_Auth.py
+++ app/utils/others/bookcase_Auth.py
@@ -3,14 +3,11 @@ from utils.others.input_Auth import inputAuth
 
 
 def bookcaseAuth(directory):
-    path = "Repeat"
-    conn_type = "Repeat"
-    while path == "Repeat" or conn_type == "Repeat":
+    path = conn_type = "Repeat"
+    while path == conn_type == "Repeat":
         path, conn_type = inputAuth()
-    if path == "Exit" or conn_type == "Exit":
-        return path, conn_type
+    if path == conn_type == "Exit":
+        return "Exit"
     else:
         encryptPath(directory, path, conn_type)
-        path = False
-        conn_type = False
-        return path, conn_type
+        return ""
