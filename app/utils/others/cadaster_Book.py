00c118387508a4248c1ccd0ed1a8b586f841f7dd
lipebol <lipe_bol5@hotmail.com>
2024-04-13 20:20:46 -0300

    update variables name and required conditions

--- app/utils/others/cadaster_Book.py
+++ app/utils/others/cadaster_Book.py
@@ -3,18 +3,23 @@ from utils.others.others_Data import othersData
 from utils.others.record_Book import recordBook
 from utils.others.search_Input import searchInput
 
+
 def cadasterBook(user_bookcase):
-    data, n, headers = searchInput()
-    URL_book = "Não"
-    if data != n != headers != "Exit":
-        while URL_book == "Não": 
-            URL_book = checkSearch(data, n, headers)
+    data, n = searchInput()
+    if data != n != "Exit":
+        url_book = "No"
+        while url_book == "No":
+            url_book = checkSearch(data, n)
             n+=1
-        if URL_book != None:
-            collection = "Repeat"
-            status = "Repeat"
-            while collection == "Repeat" or status == "Repeat":
-                collection, status = othersData()
-            if collection != "Exit" or status != "Exit":
-                result = recordBook(URL_book, collection, status, user_bookcase)
-                return result
+        if url_book != "Exit":
+            collection = acquired = ""
+            while collection == acquired == "":
+                collection, acquired = othersData()
+            if collection and acquired not in ("Exit"):
+                if type(url_book) == dict:
+                    url_book["Adquirido?"] = acquired
+                    acquired = ""
+                recordBook(url_book, collection, acquired, user_bookcase)
+    
+    return "Reload"
+    
