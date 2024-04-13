2ce06765c62c61dbe42f082247d4fd491d1080d3
lipebol <lipe_bol5@hotmail.com>
2024-04-12 23:57:37 -0300

    Add 'textwrap'

--- app/utils/others/add_Item.py
+++ app/utils/others/add_Item.py
@@ -1,13 +1,23 @@
 # import json
+import textwrap
 from utils.verifications.ack_Item import ackItem
 from utils.verifications.conn_Database import connDatabase
 from utils.verifications.extract_Two import extractTwo
 from utils.verifications.up_Item import upItem
 
 
-def addItem(URL_book, collection, status, user_bookcase):
+def addItem(url_book, collection, acquired, user_bookcase):
 
-    item = extractTwo(URL_book, status)
+    if type(url_book) != dict:
+        item = extractTwo(url_book, acquired)
+        item[" coleção "] = collection
+    else:
+        item = url_book
+        wrapperI = textwrap.TextWrapper(width=45)
+        wrapperII = textwrap.TextWrapper(width=35)
+        item["título"] = wrapperI.fill(text=item["título"])
+        item["autor(es)"] = wrapperII.fill(text=item["autor(es)"])
+        item[" coleção "] = collection
     
     MongoDB = connDatabase(user_bookcase)
     if MongoDB == "Error":
@@ -30,8 +40,8 @@ def addItem(URL_book, collection, status, user_bookcase):
             return "item_Added"
     else:
         up_item = upItem(
-            ack_item, item["categoria(s)"], 
-            item["avaliação"], item["preço"], collection
+            ack_item, item["categoria(s)"], item["loja"],
+            item["link"], item["avaliação"], item["preço"], collection
         )
         if up_item == "No Update":
             return "prev_Registered"
