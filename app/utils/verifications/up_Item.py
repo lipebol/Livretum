a3c05a06d8aebc16b049e576975bc616af9335ad
lipebol <lipe_bol5@hotmail.com>
2024-04-27 23:37:18 -0300

    add get store and link

--- app/utils/verifications/up_Item.py
+++ app/utils/verifications/up_Item.py
@@ -1,18 +1,36 @@
 
 
-def upItem(ack_item, categories, rating, price, collection):
+def upItem(ack_item, categories, store, url_purchase, rating, price, collection):
     
     locate = {
         '_id': ack_item['_id']
         }
+
     up_categories = ack_item['categoria(s)'] == categories
-    if up_categories == False:
+    if categories != "manually":
+        if up_categories == False:
+            up = {
+                "$set": {'categoria(s)': categories}
+            }
+            upCategories = collection.update_one(locate, up)
+            if upCategories.modified_count == 1:
+                up_categories = "categories"
+    up_store = ack_item['loja'] == store
+    if up_store == False:
         up = {
-            "$set": {'categoria(s)': categories}
+            "$set": {'loja': store}
         }
-        upCategories = collection.update_one(locate, up)
-        if upCategories.modified_count == 1:
-            up_categories = "categories"
+        upStore = collection.update_one(locate, up)
+        if upStore.modified_count == 1:
+            up_store = "store"
+    up_urlpurchase = ack_item['link'] == url_purchase
+    if up_urlpurchase == False:
+        up = {
+            "$set": {'link': url_purchase}
+        }
+        upUrlPurchase = collection.update_one(locate, up)
+        if upUrlPurchase.modified_count == 1:
+            up_urlpurchase = "url_purchase"
     up_rating = ack_item['avaliação'] == rating
     if up_rating == False:
         up = {
@@ -30,7 +48,7 @@ def upItem(ack_item, categories, rating, price, collection):
         if upPrice.modified_count == 1:
             up_price = "price"
     
-    if up_categories == up_rating == up_price:
+    if up_categories == up_store == up_urlpurchase == up_rating == up_price:
         return "No Update"
     else:
         return "Update"
