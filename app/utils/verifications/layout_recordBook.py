b2d033f41039cc5223905628f00384001f990886
lipebol <lipe_bol5@hotmail.com>
2024-04-27 23:35:55 -0300

    add textwrap

--- app/utils/verifications/layout_recordBook.py
+++ app/utils/verifications/layout_recordBook.py
@@ -1,11 +1,19 @@
 import PySimpleGUI as sg
+import textwrap
 from utils.verifications.extract_Two import extractTwo
 
-def layoutrecordBook(URL_book, collection, status):
+def layoutrecordBook(url_book, collection, acquired):
 
     logo = sg.Image(filename='app/src/images/Livretum.png')
     confirm_data_book = sg.Text("Dados do Livro", font='Courier')
-    item = extractTwo(URL_book, status)
+    if type(url_book) != dict:
+        item = extractTwo(url_book, acquired)
+    else:
+        item = url_book
+        wrapperI = textwrap.TextWrapper(width=45)
+        wrapperII = textwrap.TextWrapper(width=35)
+        item["título"] = wrapperI.fill(text=item["título"])
+        item["autor(es)"] = wrapperII.fill(text=item["autor(es)"])
 
     layout_recordBook = [
         [sg.Text('')],
@@ -20,9 +28,10 @@ def layoutrecordBook(URL_book, collection, status):
         [sg.Text(f'ISBN-13: {item["isbn_13"]}', font='Courier')],
         [sg.Text(f'Páginas: {item["páginas"]}', font='Courier')],
         [sg.Text(f'Coleção: {collection}', font='Courier')],
-        [sg.Text(f'Adquirido?: {item["Adquirido?"]}', font='Courier')],
+        [sg.Text(f'Loja: {item["loja"]}', font='Courier')],
         [sg.Text(f'Avaliação: {item["avaliação"]}', font='Courier')],
         [sg.Text(f'Preço: {item["preço"]}', font='Courier')],
+        [sg.Text(f'Adquirido?: {item["Adquirido?"]}', font='Courier')],
         [sg.Text('')],
         [sg.Button("Cadastrar", font='Courier')],
         [sg.Text('')]
