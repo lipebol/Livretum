890de672a3879bb6c17f2aa7a328c633fe524548
lipebol <lipe_bol5@hotmail.com>
2025-03-03 11:19:03 -0300

    rm 'notify' and add new 'checks'

--- v2/src/utils/check.py
+++ v2/src/utils/check.py
@@ -1,6 +1,7 @@
-from os import getenv
+from os import getenv, listdir
+from os.path import expanduser
 from requests import get
-from utils.notify import Notify
+from utils.sqlite import SQLite
 
 
 class Check:
@@ -9,5 +10,17 @@ class Check:
         try:
             return get(getenv('URL_GOOGLE'))
         except Exception:
-            Notify().noNet()
-            exit()
+            return False
+
+    def default_dir(self):
+        return getenv('DEFAULT_DIR') % expanduser('~')
+
+    def key(self):
+        return '.ps.key' in listdir(self.default_dir())
+
+    def db(self):
+        return 'livretum.db' in listdir(self.default_dir())
+
+    def username(self, username: str) -> list:
+        self.__user = SQLite(self.default_dir()).dql('select_user_by_name', (username,))
+        return self.__user[0] if self.__user != [] else SQLite(self.default_dir()).new_user(username)
