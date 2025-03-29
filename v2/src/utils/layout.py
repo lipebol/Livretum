48d6f1eb0b1d66a074e9bf5fc55ef2401656dd58
lipebol <lipe_bol5@hotmail.com>
2025-03-29 13:12:33 -0300

    add new

--- v2/src/utils/layout.py
+++ v2/src/utils/layout.py
@@ -34,7 +34,7 @@ class Layout:
                 ],
                 [Text('')]
             ], 
-            size=(self.size_x, self.size_y), resizable=True, grab_anywhere=True, 
+            size=(self.size_x, self.size_y), grab_anywhere=True, 
             alpha_channel=.9, location=(self.loc_x, self.loc_y)
         )
 
@@ -51,7 +51,7 @@ class Layout:
                 [Text('')],
                 [Button('Enviar',font=getenv('DEFAULT_FONT'),bind_return_key=True)]
             ],
-            size=(self.size_x, self.size_y), resizable=True, grab_anywhere=True, 
+            size=(self.size_x, self.size_y), grab_anywhere=True, 
             alpha_channel=.9, element_justification='c', location=(self.loc_x, self.loc_y)
         )
 
@@ -91,9 +91,72 @@ class Layout:
             size=(self.size_x, self.size_y), resizable=True, grab_anywhere=True, 
             alpha_channel=.9, element_justification='c', location=(self.loc_x, self.loc_y)
         )
-    
 
-    def statusAcquired(self, screen: object) -> object:
+
+    def search(self, screen: object, book=None) -> object:
+        self.size_x, self.loc_x, self.size_y, self.loc_y = tuple(screen)
+        return Window(
+            getenv('NAME'), icon=self.icon, layout=[
+                [Text('')],
+                [Column([[Image(filename=self.logo)]])],
+                [Text('')],
+                [Text('')],
+                [Column([[Text('Autor ou Assunto: ',font=getenv('DEFAULT_FONT'))]])],
+                [Column([[InputText('',key='book',size=(26),font=getenv('DEFAULT_FONT'),focus=True)]])],
+                [Text('')],
+                [Button('Pesquisar',font=getenv('DEFAULT_FONT'),bind_return_key=True)] # bind_return_key = Enter
+            ] if not book else [
+                [Text('')],
+                [Column([[Image(filename=self.logo)]],justification=getenv('DEFAULT_JUSTIFICATION'))],
+                [Text('')],
+                [
+                    Column(
+                        [[Text('É esse o livro?',font=getenv('DEFAULT_FONT'))]],
+                        justification=getenv('DEFAULT_JUSTIFICATION')
+                    )
+                ],
+                [
+                    Column(
+                        [[Image(filename=book.get('image'))]],
+                        justification=getenv('DEFAULT_JUSTIFICATION')
+                    )
+                ],
+                [Text('')],
+                [Column([[Text(f'Nome: {book.get('title')} ',font=getenv('DEFAULT_FONT'))]])],
+                [Column([[Text(f'Autor(es): {book.get('authors')} ',font=getenv('DEFAULT_FONT'))]])],
+                [
+                    Column(
+                        [[
+                            Text(
+                                f'ISBN-10: {book['id'].get('isbn_10') if book.get('id') != '' else None}',
+                                font=getenv('DEFAULT_FONT')
+                            )
+                        ]]
+                    ),
+                    Column(
+                        [[
+                            Text(f"ISBN-13: {book['id'].get('isbn_13') if book.get('id') != '' else None} ", 
+                                 font=getenv('DEFAULT_FONT')
+                            )
+                        ]]
+                    )
+                ],
+                [Text('')],
+                [
+                    Column(
+                        [[Button('Sim',font=getenv('DEFAULT_FONT'))]],
+                        justification=getenv('DEFAULT_JUSTIFICATION')
+                    ),
+                    Column([[Button('Não',font=getenv('DEFAULT_FONT'))]])
+                ],
+                [Text('')]
+            ],
+            size=(self.size_x, self.size_y), grab_anywhere=True, 
+            element_justification='c' if not book else None, location=(self.loc_x, self.loc_y)
+        )
+
+    
+    def acquired(self, screen: object, book=None) -> object:
         self.size_x, self.loc_x, self.size_y, self.loc_y = tuple(screen)
         return Window(
             getenv('NAME'), icon=self.icon, layout=[
@@ -103,26 +166,11 @@ class Layout:
                 [Column([[InputText('',key='id',size=(15),font=getenv('DEFAULT_FONT'),focus=True)]])],
                 [Text('')],
                 [Button('Enviar',font=getenv('DEFAULT_FONT'),bind_return_key=True)]
-            ],
-            size=(self.size_x, self.size_y), resizable=True, grab_anywhere=True, 
-            element_justification='c', location=(self.loc_x, self.loc_y)
-        )
-
-    
-    def thisId(self, screen: object, book: list) -> object:
-        self.size_x, self.loc_x, self.size_y, self.loc_y = tuple(screen)
-        return Window(
-            getenv('NAME'), icon=self.icon, layout=[
+            ] if not book else [
                 [Text('')],
-                [
-                    Column([[Text('[!] Você adquiriu o livro abaixo:',font=getenv('DEFAULT_FONT'))]])
-                ],
-                [
-                    Column([[Text(f'Título: {book[2]}',font=getenv('DEFAULT_FONT'))]])
-                ],
-                [
-                    Column([[Text(f'Autor(es): {book[3]}',font=getenv('DEFAULT_FONT'))]])
-                ],
+                [Column([[Text('[!] Você adquiriu o livro abaixo:',font=getenv('DEFAULT_FONT'))]])],
+                [Column([[Text(f'Título: {book[2]}',font=getenv('DEFAULT_FONT'))]])],
+                [Column([[Text(f'Autor(es): {book[3]}',font=getenv('DEFAULT_FONT'))]])],
                 [Column([[Button('Confirmar',font=getenv('DEFAULT_FONT'))]])],
             ],
             size=(self.size_x, self.size_y), grab_anywhere=True, 
