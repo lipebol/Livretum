779e2489008fba6675ac438c12874379ce6a1073
lipebol <lipe_bol5@hotmail.com>
2025-03-29 13:14:54 -0300

    rewritten

--- v2/src/utils/others.py
+++ v2/src/utils/others.py
@@ -1,48 +1,78 @@
 from PySimpleGUI import WIN_CLOSED
 
+from time import sleep
 
 class Others:
 
     def __init__(self, Set: object, Layout: object) -> None:
         self.__set, self.__layout = Set, Layout
 
-    def statusAcquired(self) -> str:
+    def search(self) -> str:
+        def choose(book: dict):
+            try:
+                chooseWindow = self.__layout.search(
+                    self.__set.screen((0.43923865300146414, 0.859375)),
+                    self.__set.search(book)
+                )
+                while True:
+                    event, values = chooseWindow.read()
+                    if event == WIN_CLOSED:
+                        return 'Exit'
+                    if event == 'Sim':
+                        return True
+                    if event == 'Não':
+                        chooseWindow.Hide()
+                        return None 
+            except Exception as error:
+                print(f"{error} in 'Others.search.choose'")
+        try:  
+            self.searchWindow = self.__layout.search(
+                self.__set.screen((0.29282576866764276, 0.46875))
+            )
+            while True:
+                event, values = self.searchWindow.read()
+                if event == WIN_CLOSED:
+                    return 'Reload'
+                if event == 'Pesquisar':
+                    if values['book']:
+                        for book in self.__set.search(values['book']):
+                            if book:
+                                if choose(book):
+                                    break
+                        print(book)
+                            #print('não tem nada!')       
+        except Exception as error:
+            print(f"{error} in 'Others.search'")
+
+    def acquired(self) -> str:
+        def confirm(id: str) -> str:
+            try:
+                confirmWindow = self.__layout.acquired(
+                    self.__set.screen((0.3953147877013177, 0.2734375)), 
+                    self.__set.bookcase().values.tolist()[int(id)-1]
+                )
+                while True:
+                    event, values = confirmWindow.read()
+                    if event == WIN_CLOSED:
+                        return None
+                    if event == 'Confirmar':
+                        confirmWindow.Hide()
+                        return self.__set.acquired(id)
+            except Exception as error:
+                print(f"{error} in 'Others.acquired.confirm'")
         try:
-            self.statusAcquiredWindow = self.__layout.statusAcquired(
+            self.acquiredWindow = self.__layout.acquired(
                 self.__set.screen((0.24890190336749635, 0.2734375))
             )
             while True:
-                event, values = self.statusAcquiredWindow.read()
+                event, values = self.acquiredWindow.read()
                 if event == WIN_CLOSED:
-                    return 'Exit'
-                    break
+                    return None
                 if event == 'Enviar':
                     if values['id'] and values['id'].isdigit():
                         if int(values['id']) <= len(self.__set.bookcase().values.tolist()):
                             if self.__set.bookcase().values.tolist()[int(values['id'])-1][-1] != 'S':
-                                self.statusAcquiredWindow.Hide()
-                                return self.thisId(values['id'])
-                                break
+                                self.acquiredWindow.Hide()
+                                return confirm(values['id'])
         except Exception as error:
-            print(error)
-            return 'Error'
-
-
-    def thisId(self, id: str) -> str:
-        try:
-            self.thisIdWindow = self.__layout.thisId(
-                self.__set.screen((0.3953147877013177, 0.2734375)), 
-                self.__set.bookcase().values.tolist()[int(id)-1]
-            )
-            while True:
-                event, values = self.thisIdWindow.read()
-                if event == WIN_CLOSED:
-                    return 'Exit'
-                    break
-                if event == 'Confirmar':
-                    self.thisIdWindow.Hide()
-                    return self.__set.updateAcquired(id)
-                    break
-        except Exception as error:
-            print(error)
-            return 'Error'
+            print(f"{error} in 'Others.acquired'")
